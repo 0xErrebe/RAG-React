6c88b9f1bff8fa495e83ce4cbb427e4f7f6aaf04
beilord <raul_bch@hotmail.com>
2024-11-12 13:20:20 +0100

    Change anchor url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import '../styles/Footer.css'
+import '../styles/footer.css'
 
 export default function Footer() {
   return (
     <footer>
       <h3>Local and private RAG</h3>
       <p>Made with 💖 by <a href="https://github.com/0xErrebe">0xErrebe</a></p>
-      <a href="https://github.com/0xErrebe/thunderstore.io-downloader">Project Repo</a>
+      <a href="https://github.com/0xErrebe/RAG-React">Project Repo</a>
     </footer>
   )
 }
